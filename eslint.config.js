0f9f715a9dc0352cce4c2723147d9b3a935b4604
Enzo Falcon <falcone134@gmail.com>
2024-10-17 14:08:56 -0500

    Add eslint checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,18 @@
-import withNuxt from './.playground/.nuxt/eslint.config.mjs'
+import withNuxt from "./.playground/.nuxt/eslint.config.mjs";
 
-export default withNuxt()
+export default withNuxt(
+  // Your custom configs here
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "vue/max-attributes-per-line": [
+        "error",
+        {
+          singleline: { max: 3 },
+          multiline: { max: 1 },
+        },
+      ],
+    },
+  },
+);
